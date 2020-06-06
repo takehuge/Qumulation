872ef179324da02accb98cfd8f7f9ba33049f474
David Lee <ufocrew@gmail.com>
2020-06-06 11:34:20 +0800

    Update QM

--- QuManipulate.py
+++ QuManipulate.py
@@ -2,17 +2,21 @@ import matplotlib
 matplotlib.use('tkAgg')
 
 from qutip import *
-from scipy import *
-from numpy import *
+from scipy import linspace, pi
+from numpy import array, mean, sqrt, arange
+
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 # Config Bloch Sphere
 # azim: camera-right's deg from y-axis, elev: camera-up's deg from horizontal
 fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(1, 1, 1, projection='3d')
-ax.set_aspect("equal")
+
+# ax.set_aspect("equal") # NOT implemented in the latest Matplotlib
+
 sphere = Bloch(axes=ax, view=[-25,12]) # view=[azim,elev]
 # sphere.frame_color = 'blue'
 # sphere.frame_width = 1.5
@@ -35,6 +39,7 @@ x2, y2, z2 =[], [], []
 xh, yh, zh =[], [], []
 xhh, yhh, zhh =[], [], []
 xH, yH, zH =[], [], []
+xHH, yHH, zHH =[], [], []
 
 # Y-axis Rotation
 Inistate = basis(2, 0) #snot(1)*basis(2, 0)
@@ -71,7 +76,7 @@ for j in rotrange2:
     yh.append(expect(sigmay(), ry(j * -pi / 100, 1, 0) * rx(-pi, 1) * Inistate))
     zh.append(expect(sigmaz(), ry(j * -pi / 100, 1, 0) * rx(-pi, 1) * Inistate))
 
-# 2nd H Rotation
+# 2nd H Rotation (back)
 Inistate = ry(-pi / 2, 1) * rx(-pi, 1) * basis(2, 0)
 for j in rotrange:
     xhh.append(expect(sigmax(), rx(j * -pi / 100, 1, 0) * Inistate))
@@ -83,12 +88,23 @@ for j in rotrange2:
     zhh.append(expect(sigmaz(), ry(j * -pi / 100, 1, 0) * rx(pi, 1) * Inistate))
 
 # H-axis Rotation
+Inistate = basis(2, 0) #snot(1)*basis(2, 0)
 for j in rotrange:
     xH.append(expect(sigmax(), ((rx(j * -pi / 100, 1, 0) +
                                  rz(j * -pi / 100, 1, 0)) * Inistate).unit()))
     yH.append(expect(sigmay(), ((rx(j * -pi / 100, 1, 0) +
                                  rz(j * -pi / 100, 1, 0)) * Inistate).unit()))
     zH.append(expect(sigmaz(), ((rx(j * -pi / 100, 1, 0) +
+                                 rz(j * -pi / 100, 1, 0)) * Inistate).unit()))
+
+# 2nd H-axis Rotation (back)
+Inistate = ry(-pi / 2, 1) * rx(-pi, 1) * basis(2, 0)
+for j in rotrange:
+    xHH.append(expect(sigmax(), ((rx(j * -pi / 100, 1, 0) +
+                                 rz(j * -pi / 100, 1, 0)) * Inistate).unit()))
+    yHH.append(expect(sigmay(), ((rx(j * -pi / 100, 1, 0) +
+                                 rz(j * -pi / 100, 1, 0)) * Inistate).unit()))
+    zHH.append(expect(sigmaz(), ((rx(j * -pi / 100, 1, 0) +
                                  rz(j * -pi / 100, 1, 0)) * Inistate).unit()))
 
 R = sqrt(array(x)**2 + array(y)**2 + array(z)**2)
@@ -104,8 +120,8 @@ def animate(j):
     # sphere.make_sphere()
     # 2nd Hadamard
     sphere.clear()
-    sphere.add_points([xhh[:j+1], yhh[:j+1], zhh[:j+1]])
-    sphere.add_vectors([xhh[j], yhh[j], zhh[j]])
+    sphere.add_points([xH[:j+1], yH[:j+1], zH[:j+1]])
+    sphere.add_vectors([xH[j], yH[j], zH[j]])
     sphere.make_sphere()
     return sphere
 
@@ -117,11 +133,23 @@ def init():
     return ax
 
 # animation starts
-framerange = arange(len(rotrange) + len(rotrange2)) # for X, Y, Z gate
+framerange = arange(len(rotrange)) # for H-axis gate
+# framerange = arange(len(rotrange) + len(rotrange2)) # for X, Y, Z gate
 # framerange = arange(t_length) + int((t_length + 1)/2) # for Hback gate
 ani = animation.FuncAnimation(fig, animate, framerange,
                               init_func=init, interval=25, repeat=False, blit=False)
 #Install FFMPEG using: brew install ffmpeg
-ani.save('Qubitshh.mp4', fps=15)
+ani.save('QubitsH.mp4', fps=15)
+
+# visually plotting out:
+X, Y, Z = xhh, yhh, zhh
+max_range = array([max(X)-min(X), max(Y)-min(Y), max(Z)-min(Z)]).max() / 2.0
+
+mid_x = (max(X)+min(X)) * 0.5
+mid_y = (max(Y)+min(Y)) * 0.5
+mid_z = (max(Z)+min(Z)) * 0.5
+ax.set_xlim(mid_x - max_range, mid_x + max_range)
+ax.set_ylim(mid_y - max_range, mid_y + max_range)
+ax.set_zlim(mid_z - max_range, mid_z + max_range)
 
 plt.show()
